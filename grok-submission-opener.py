7bcca2b56dfba3e6285043a0fb2b362b3ec82849
misskacie <121023187+misskacie@users.noreply.github.com>
2024-10-24 19:00:59 +1100

    clean up

--- grok-submission-opener.py
+++ grok-submission-opener.py
@@ -1,6 +1,6 @@
 '''
 Simple script to open every submission from the grok marking page HTML file.
-Just right click, hit save as into same location as this, and run this.
+Right click page, hit save as, save into same location as this, and run this.
 '''
 from bs4 import BeautifulSoup
 import webbrowser  
@@ -8,10 +8,9 @@ import webbrowser
 with open("Grok Academy.html") as fp:
     soup = BeautifulSoup(fp,'lxml') 
  
-# comment out if you want to use default browser
+# remove the string for default browswer
 browser = webbrowser.get('chromium')  
-for h in soup.findAll('td'): 
-    a = h.find('a') 
+for a in soup.findAll('a'): 
     try:  
         if 'href' in a.attrs: 
             url = a.get('href')
@@ -19,4 +18,3 @@ for h in soup.findAll('td'):
                 browser.open(url, new=2, autoraise=False) 
     except Exception: 
         pass
-
